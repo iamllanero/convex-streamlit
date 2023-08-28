2849142cfe8b2358f582ea35c2a5dc6302a3d621
Llanero <iamllanero@gmail.com>
2023-08-27 20:56:28 -0500

    Clean up unused files

--- bribers.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-bribes_df = pd.read_csv('data/rounds-bribes.csv')
-bribes = list(bribes_df.columns)
-bribes.remove('round')
-with open('data/bribers.csv', 'w') as f:
-    for bribe in bribes:
-        f.write(bribe)
-        f.write('\n')
